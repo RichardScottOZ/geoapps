0d78da3282b76b8b0f7dffd598310db602187dfe
benk-mira <benjamink@mirageoscience.com>
2021-03-23 15:36:31 -0700

    test linter and testing for inversion

---
+++ tests/inversion_test.py
@@ -0,0 +1,13 @@
+#  Copyright (c) 2021 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from geoh5py.workspace import Workspace
+
+
+def test_inversion():
+    """ test this fnction"""
+    aaabbbcccdddeeefffggghhhiii = 12
